5b912a3d9e9c7f54a32ed0ae5a73756422e47aca
Kyoko Sakuma <hzk.butterfly459@gmail.com>
2021-03-04 03:26:13 +0900

    succeed scene management

--- Assets/GameSystem.cs
+++ Assets/GameSystem.cs
@@ -1,11 +1,13 @@
-using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
  
 public class GameSystem : MonoBehaviour {
  
     //　スタートボタンを押したら実行する
-    public void StartGame() {
+    public void OnClick() {
         SceneManager.LoadScene ("WorldInteractionDemo");
     }
 }
