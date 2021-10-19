d685ebd97ddbb27b74f6f5173f4a16bf12ab94ff
Kyoko Sakuma <hzk.butterfly459@gmail.com>
2021-10-20 05:08:44 +0900

    add lock ui panel function

---
+++ Assets/LockPanel.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockPanel : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject uipanel;
+    
+    [SerializeField]
+    private GameObject uiparent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        if (uipanel.transform.parent == null)
+        {
+            uipanel.transform.parent = uiparent.transform;
+        }
+        else
+        {
+            uipanel.transform.parent = null;
+        }
+    }
+}
--- Assets/frameOnOff.cs
+++ Assets/frameOnOff.cs
@@ -10,7 +10,7 @@ public class frameOnOff : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        frame.SetActive(false);
     }
 
     // Update is called once per frame
