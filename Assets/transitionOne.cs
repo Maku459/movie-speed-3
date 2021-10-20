3f2af5b755771468f6c55bbcb58d16d26c519b14
Kyoko Sakuma <hzk.butterfly459@gmail.com>
2021-10-21 06:51:51 +0900

    control fix

--- Assets/transitionOne.cs
+++ Assets/transitionOne.cs
@@ -17,9 +17,11 @@ public class transitionOne : MonoBehaviour {
     // イベントハンドラー（イベント発生時に動かしたい処理）
     void SceneLoaded (Scene nextScene, LoadSceneMode mode) {
         var g = GameObject.Find("Video Player").GetComponent<VideoPlayer>();
-        float[] speedArray = new float[9] {0.8f, 0.85f, 0.9f, 0.95f, 1.0f, 1.05f, 1.1f, 1.15f, 1.2f};
+        //float[] speedArray = new float[9] {0.8f, 0.85f, 0.9f, 0.95f, 1.0f, 1.05f, 1.1f, 1.15f, 1.2f};
+        float[] speedArray = new float[8] {0.5f, 0.6f, 0.7f, 0.8f, 1.2f, 1.3f, 1.4f, 1.5f};
+        
         //小数点以下第二位
-        var firstSpeed = speedArray[Random.Range(0,9)];
+        var firstSpeed = speedArray[Random.Range(0,8)];
         g.playbackSpeed = firstSpeed;
         Debug.Log(g.playbackSpeed);
     }
