91571b061b0861c7c063fd99286cbbaebdcdf343
Kyoko Sakuma <hzk.butterfly459@gmail.com>
2021-10-31 04:24:18 +0900

    frame

--- Assets/frameOnOff.cs
+++ Assets/frameOnOff.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
 
 public class frameOnOff : MonoBehaviour
 {
     [SerializeField]
+    private GameObject subcam;
+    
+    [SerializeField]
     private GameObject frame;
+    
+    [SerializeField]
+    private GameObject dummyframe;
+
+    [SerializeField]
+    private GameObject rawimg;
     
     // Start is called before the first frame update
     void Start()
     {
+        subcam.SetActive(false);
         frame.SetActive(false);
+        dummyframe.SetActive(false);
+        rawimg.SetActive(false);
     }
 
     // Update is called once per frame
@@ -18,11 +31,17 @@ public class frameOnOff : MonoBehaviour
     {
         if (frame.activeSelf)
         {
+            subcam.SetActive(false);
             frame.SetActive(false);
+            dummyframe.SetActive(false);
+            rawimg.SetActive(false);
         }
         else
         {
+            subcam.SetActive(true);
             frame.SetActive(true);
+            dummyframe.SetActive(true);
+            rawimg.SetActive(true);
         }
     }
 }
